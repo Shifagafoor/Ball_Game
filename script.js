61fad54f2232db5a1757cb1f3e4081c54a2bb12e
Shifa Gafoor A <shifagafoorncas@gmail.com>
2025-01-12 20:03:08 +0530

    .

--- script.js
+++ script.js
@@ -7,16 +7,13 @@ balls.forEach(ball => {
     });
 });
 
-// Add event listeners to each container
 containers.forEach(container => {
     container.addEventListener('click', () => {
         moveBallBetweenContainers(container);
     });
 });
 
-// Function to move a ball to the first container with space
 function moveBallToContainer(ball) {
-    // Find the first container with space
     for (let container of containers) {
         if (container.children.length < 10) {
             container.appendChild(ball);
@@ -26,9 +23,7 @@ function moveBallToContainer(ball) {
     }
 }
 
-// Function to move a ball from the first non-empty container to the clicked container
 function moveBallBetweenContainers(clickedContainer) {
-    // Find the first non-empty container
     let sourceContainer;
     for (let container of containers) {
         if (container.children.length > 0) {
@@ -37,7 +32,6 @@ function moveBallBetweenContainers(clickedContainer) {
         }
     }
 
-    // Move the last ball from the source container to the clicked container
     if (sourceContainer && sourceContainer !== clickedContainer && clickedContainer.children.length < 10) {
         const ball = sourceContainer.lastElementChild;
         clickedContainer.appendChild(ball);
