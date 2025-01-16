015f8e56de0f7702c3495412c0831b258ae15682
Shifa Gafoor A <shifagafoorncas@gmail.com>
2025-01-16 10:55:38 +0530

    .

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
-const balls = document.querySelectorAll('.numbers');
+const screens = document.querySelectorAll('.screen');
+const start_btn = document.getElementById('start-btn')
+const balls = document.querySelectorAll('.number');
 const containers = document.querySelectorAll('.container');
+
+
+start_btn.addEventListener('click', () => 
+    screens[0].classList.add('up'))
+
 
 let selectedContainer = null;
 let selectedBall = null;
 
 containers.forEach(container => {
-    container.addEventListener('click' , () => {
-        selectedContainer =container;
-
-        console.log(selectedContainer)
+    container.addEventListener('click', () => {
+        selectedContainer = container;
+        console.log('Selected container:', selectedContainer);
     });
 });
 
 balls.forEach(ball => {
-    balls.addEventListener('click', () => {
+    ball.addEventListener('click', () => {
         if (selectedContainer) {
             selectedContainer.appendChild(ball);
-            selectedContainer = null;
+            selectedContainer = null; 
         } else {
             selectedBall = ball;
         }
     });
 });
+
 containers.forEach(container => {
-    container.addEventListener('click' , () => {
-        if(selectedBall) {
+    container.addEventListener('click', () => {
+        if (selectedBall) {
             container.appendChild(selectedBall);
-            selectedBall = null;
+            selectedBall = null; 
         }
     });
 });
