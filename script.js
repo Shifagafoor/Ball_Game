b7d4d190278018b0b7bee28c8b906f8d4af81e08
Shifa Gafoor A <shifagafoorncas@gmail.com>
2025-01-16 09:26:46 +0530

    .

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 const balls = document.querySelectorAll('.number');
 const containers = document.querySelectorAll('.container');
 
-balls.forEach(ball => {
-    ball.addEventListener('click', () => {
-        selectedBall = ball; 
-        console.log(selectedBall)
+let selectedContainer = null;
+let selectedBall = null;
+
+containers.forEach(container => {
+    container.addEventListener('click', () => {
+        selectedContainer = container;
+        console.log('Selected container:', selectedContainer);
     });
 });
 
-let selectedBall = null;
+balls.forEach(ball => {
+    ball.addEventListener('click', () => {
+        if (selectedContainer) {
+            selectedContainer.appendChild(ball);
+            selectedContainer = null; 
+        } else {
+            selectedBall = ball; 
+        }
+    });
+});
+
 containers.forEach(container => {
     container.addEventListener('click', () => {
         if (selectedBall) {
-            container.insertBefore(selectedBall, container.firstChild);
-            selectedBall = null;
-        }else{
-            let cont = container.firstChild;
-
-            containers.forEach((jar) => {
-                
-                jar.addEventListener('click' , () => {
-                    if (cont && cont instanceof Node) {
-
-                        jar.appendChild(cont)
-                        cont = null;
-                    }    
-                })
-                console.log(jar)
-            })
+            container.appendChild(selectedBall);
+            selectedBall = null; 
         }
     });
 });
