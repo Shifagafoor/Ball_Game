e9df35d6b7228087a89aa54551e670037b4c293b
Shifa Gafoor A <shifagafoorncas@gmail.com>
2025-02-06 09:42:50 +0530

    .

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
-const screens = document.querySelectorAll('.screen');
-const start_btn = document.getElementById('start-btn')
-const balls = document.querySelectorAll('.number');
-const containers = document.querySelectorAll('.container');
+const screens = document.querySelectorAll(".screen");
+const start_btn = document.getElementById("start-btn");
+const balls = document.querySelectorAll(".number");
+const containers = document.querySelectorAll(".container");
+const restartBtn = document.getElementById("restart-btn");
 
-
-start_btn.addEventListener('click', () => 
-    screens[0].classList.add('up'))
-
+start_btn.addEventListener("click", () => screens[0].classList.add("up"));
 
 let selectedContainer = null;
 let selectedBall = null;
 
-containers.forEach(container => {
-    container.addEventListener('click', () => {
-        selectedContainer = container;
-        // console.log('Selected container:', selectedContainer);
-    });
+containers.forEach((container) => {
+  container.addEventListener("click", () => {
+    selectedContainer = container;
+  });
 });
 
-balls.forEach(ball => {
-    ball.addEventListener('click', () => {
-        if (selectedContainer) {
-            selectedContainer.appendChild(ball);
-            selectedContainer = null; 
-        } else {
-            selectedBall = ball;
-        }
-    });
+balls.forEach((ball) => {
+  ball.addEventListener("click", () => {
+    if (selectedContainer) {
+      selectedContainer.appendChild(ball);
+      selectedContainer = null;
+    } else {
+      selectedBall = ball;
+    }
+  });
 });
 
-containers.forEach(container => {
-    container.addEventListener('click', () => {
-        if (selectedBall) {
-            container.appendChild(selectedBall);
-            selectedBall = null; 
-        }
-    });
+containers.forEach((container) => {
+  container.addEventListener("click", () => {
+    if (selectedBall) {
+      container.appendChild(selectedBall);
+      selectedBall = null;
+    }
+  });
+});
+
+restartBtn.addEventListener("click", () => {
+  selectedBall = null;
+  selectedContainer = null;
+
+  const ballsContainer = document.querySelectorAll(".container .ball");
+
+  ballsContainer.forEach((ball) => {
+
+    console.log(ball);
+  });
+
 });
